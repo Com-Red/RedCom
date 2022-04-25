2c8cae757682cd4a1517a8df187f13762f6e8199
Com-Red <98872839+Com-Red@users.noreply.github.com>
2022-04-25 16:58:39 +0500

    123

--- index.jsx
+++ index.jsx
@@ -3,13 +3,13 @@ ReactDOM.createRoot(document.getElementById("app"))
     <div id="nav">
             <ul class="nav">
             <li><a href="\index.html">Главная</a></li>
-            <li><a href="\page.html">Блог</a></li>
-            <li><a href="\test.html">Блог</a></li>
+            <li><a href="\pages\2pribora.html">2 прибора учета</a></li>
+            <li><a href="\pages\4pribora.html">4 прибора учета</a></li>
         </ul>
     </div>
 );
 
 ReactDOM.createRoot(document.getElementById("app2"))
 .render(
-    <h1>123</h1>
+    
 )
