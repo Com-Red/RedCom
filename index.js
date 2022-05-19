6f61dd976db050ba026462428118886d1293cc30
Com-Red <98872839+Com-Red@users.noreply.github.com>
2022-05-19 15:32:35 +0500

    123

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+// Главный див
 let divtop = document.querySelector('#divtop');
+// Меню
 let divmenu = document.createElement('div');
 divmenu.classList = 'divmenu';
 let menu = `
 <ul>
-    <li id='page1'>Главная</li>
-    <li><a id='page2'>Инфо</a></li>
+    <li id='page1' class='123'>Главная</li>
+    <li id='pageinfo'>Инфо</li>
     <li>
       Классы
       <ul>
-        <li>11 класс</li>
-        <li>10 класс</li>
-        <li>9 классы</li>
+        <li id='pageclass11'>11 класс</li>
+        <li id='pageclass10'>10 класс</li>
+        <li id='pageclass9'>9 классы</li>
       </ul>
     </li>
     <li>Blog
@@ -21,31 +23,28 @@ let menu = `
     </li>
     <li>Contact</li>
   </ul>`;
-divtop.insertAdjacentElement('afterbegin', divmenu)
-divmenu.insertAdjacentHTML('afterbegin', menu)
+divtop.insertAdjacentElement('afterbegin', divmenu);
+divmenu.insertAdjacentHTML('afterbegin', menu);
 
-// разное
-
-function xet (){
-  console.log(123)
-}
-
-const xer = document.querySelector('#page2')
-xer.classList = 'page2';
-let x = xer.classList.value;
-
-function fear(){
-  y = x;
-  if (y=='page2') return console.log('yes');
-  else console.log('no')
-}
-
-
-console.log(fear());
-
-
-xer.addEventListener('click', fear);
-
+// Элементы меню
 const page1 = document.querySelector('#page1');
-page1.addEventListener('click', xet);
+const pageinfo = document.querySelector('#pageinfo');
+const pageclass9 = document.querySelector('#pageclass9');
+const pageclass10 = document.querySelector('#pageclass10'); 
+const pageclass11 = document.querySelector('#pageclass11');
+// Действия меню
+function menu1 (e){
+  if (e.target.id=='page1') {console.log('page1');}
+  else if (e.target.id=='pageinfo') {console.log('pageinfo');}
+  else if (e.target.id=='pageclass9') {console.log('pageclass9')}
+  else if (e.target.id=='pageclass10') {console.log('pageclass10')}
+  else if (e.target.id=='pageclass11') {console.log('pageclass11')}  
+  else console.log('NO')
+}
+// Обработчики событий
+page1.addEventListener('click', menu1);
+pageinfo.addEventListener('click', menu1);
+pageclass9.addEventListener('click', menu1);
+pageclass10.addEventListener('click', menu1);
+pageclass11.addEventListener('click', menu1);
 
