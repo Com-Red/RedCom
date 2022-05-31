5891ab81999a2cc31596793e67900f14aabc61f8
Com-Red <98872839+Com-Red@users.noreply.github.com>
2022-05-31 12:05:09 +0500

    123

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import menu from "./test.js";
+import menu from "./JS/content.js";
 
 // Главный див
 const divtop = document.querySelector('#divtop');
@@ -17,6 +17,7 @@ const pageinfo = document.querySelector('#pageinfo');
 const pageclass9 = document.querySelector('#pageclass9');
 const pageclass10 = document.querySelector('#pageclass10'); 
 const pageclass11 = document.querySelector('#pageclass11');
+
 // Действия меню
 function menu1 (e){
   if (e.target.id=='page1') {console.log('page1');}
@@ -26,6 +27,7 @@ function menu1 (e){
   else if (e.target.id=='pageclass11') {console.log('pageclass11')}  
   else console.log('NO')
 }
+
 // Обработчики событий
 page1.addEventListener('click', menu1);
 pageinfo.addEventListener('click', menu1);
@@ -34,10 +36,10 @@ pageclass10.addEventListener('click', menu1);
 pageclass11.addEventListener('click', menu1);
 
 //Блок по умолчанию
-let but = document.createElement('button');
+const but = document.createElement('button');
 but.textContent = 'APP';
 divtop.insertAdjacentElement('afterend', but);
-let but2 = document.createElement('button');
+const but2 = document.createElement('button');
 but2.textContent = 'DEL';
 divtop.insertAdjacentElement('afterend', but2);
 
@@ -53,7 +55,7 @@ function vopros(){
     divtop.insertAdjacentElement('afterend', divtest);
   }
 };
-
+// Разное
 but.addEventListener('click', vopros);
 
 but2.addEventListener('click', function del (){
